6aa11afc93b498e7f8df600ea032c08f47a5f09b
AngelGuale <adguale@espol.edu.ec>
2016-01-28 16:25:21 -0500

    script en bash para correr los experimentos iobound 1 core, 2 cores, 8 cores

---
+++ iobound1core.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+time br -m "localhost" -r
+/home/gesuwall/ESPOL/operativos/bashreduce/toupper.py -i
+/home/gesuwall/ESPOL/operativos/bashreduce/allwords.txt -o
+/home/gesuwall/ESPOL/operativos/bashreduce/out.txt
+
---
+++ iobound8cores.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+time br -m "localhost localhost localhost localhost localhost localhost localhost localhost " -r
+/home/gesuwall/ESPOL/operativos/bashreduce/toupper.py -i
+/home/gesuwall/ESPOL/operativos/bashreduce/allwords.txt -o
+/home/gesuwall/ESPOL/operativos/bashreduce/out.txt
