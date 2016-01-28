f723e2230027f215dad482e18fd17a99121f5219
Gabriel Aumala <gabrielfima@live.com>
2016-01-28 15:25:54 -0500

    Fix error en script cpubound

--- cpubound.sh
+++ cpubound.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-time br -m "localhost" -r
-/home/gesuwall/ESPOL/operativos/bashreduce/sha512.py -i
-/home/gesuwall/ESPOL/operativos/bashreduce/allwords.txt -o
-/home/gesuwall/ESPOL/operativos/bashreduce/out.txt
+br -m "localhost" -r /home/gesuwall/ESPOL/operativos/bashreduce/toupper.py -i /home/gesuwall/ESPOL/operativos/bashreduce/allwords.txt -o /home/gesuwall/ESPOL/operativos/bashreduce/out.txt
 
